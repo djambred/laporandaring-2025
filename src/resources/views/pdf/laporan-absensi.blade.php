92c644f0ef58526171db3ae9d8f829247e2f4536
djambred <djambred@gmail.com>
2025-12-06 22:03:13 +0800

    fresh from oven

--- src/resources/views/pdf/laporan-absensi.blade.php
+++ src/resources/views/pdf/laporan-absensi.blade.php
@@ -400,9 +400,9 @@
     </div>
 
     <!-- Catatan -->
-    <div style="margin-top: 30px; padding: 15px; background: #f9f9f9; border-left: 4px solid;">
+    <div style="margin-top: 30px; padding: 15px; background: #f9f9f9;">
         <h4 style="margin-top: 0;">Catatan:</h4>
-        <ol style="margin: 10px 0; padding-left: 20px; line-height: 0;">
+        <ol style="margin: 10px 0; padding-left: 20px; line-height: 1.8;">
             <li>Dosen menyediakan link zoom perkuliahan dan mengundang mahasiswa dalam perkuliahan daring</li>
             <li>Perkuliahan direkam (jika bisa) dan tangkap layar untuk dokumentasi</li>
             <li>Perkuliahan dilaksanakan sesuai waktu kuliah luring atau disesuaikan dengan situasi</li>
